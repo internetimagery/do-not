9efff4f2868e7fc9a0bd056ed0213599f28d6ba3
kiwislob <kiwislob@gmail.com>
2021-09-17 12:28:12 +1200

    adding constants to stub file

--- donot/__init__.pyi
+++ donot/__init__.pyi
@@ -1,3 +1,9 @@
 import typing as _typ
 
+
+MAP: _typ.Final[str]
+FLATMAP: _typ.Final[str]
+FILTER: _typ.Final[str]
+HANDLER_NAMES: _typ.Final[_typ.Tuple[str, str, str]]
+
 def do(generator: _typ.Generator) -> _typ.Any: ...
